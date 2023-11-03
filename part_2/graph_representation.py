e58cf10994925d7d5e244fb30643adb3a0907654
Mody El Sayed <mody.elsayed555@gmail.com>
2023-11-03 20:58:29 +0200

    implementing dijkstra algorithm without using heap

--- part_2/graph_representation.py
+++ part_2/graph_representation.py
@@ -1,162 +1,203 @@
 # Built-In Imports
+from __future__ import annotations
 from collections import defaultdict
 from abc import ABC, abstractmethod
 
 
 class GraphAbstractRepresentation(ABC):
-	def __init__(self, directed=False, weight=1):
-		"""
-		Fundamental graph representation class
-		:param directed: flag to indicate if the graph is directed
-		:param weight: weight of the graph edges
-		"""
-		self.graph = defaultdict(dict) | defaultdict(list)
-		self.directed = directed
-		self.weighted = weight
+    def __init__(self, directed=False, weighted=1):
+        """
+        Fundamental graph representation class
+        :param directed: flag to indicate if the graph is directed
+        :param weighted: weight of the graph edges
+        """
+        self.graph = defaultdict(dict) | defaultdict(list)
+        self.directed = directed
+        self.weighted = weighted
 
-	@abstractmethod
-	def add_edge(self, u, v, weight=1):
-		pass
+    @abstractmethod
+    def add_edge(self, u, v, weight=1):
+        pass
 
-	@abstractmethod
-	def add_vertex(self, v):
-		pass
+    @abstractmethod
+    def add_vertex(self, v):
+        pass
 
-	@abstractmethod
-	def remove_edge(self, u, v):
-		pass
+    @abstractmethod
+    def remove_edge(self, u, v):
+        pass
 
-	@abstractmethod
-	def get_neighbors(self, u):
-		pass
+    @abstractmethod
+    def get_edge_weight(self, u, v):
+        pass
 
-	@abstractmethod
-	def reverse_graph(self):
-		pass
+    @abstractmethod
+    def get_neighbors(self, u, weights_only=False):
+        pass
 
-	def reverse_helper(self, reversed_graph):
-		if not self.directed:
-			raise ValueError("Graph is not directed")
+    @abstractmethod
+    def reverse_graph(self):
+        pass
 
-		for v in self.graph.keys():
-			if v not in reversed_graph.graph:
-				reversed_graph.add_vertex(v)
+    def reverse_helper(self, reversed_graph):
+        if not self.directed:
+            raise ValueError("Graph is not directed")
 
-			for u in self.get_neighbors(v):
-				reversed_graph.add_edge(u, v)
+        for v in self.graph.keys():
+            if v not in reversed_graph.graph:
+                reversed_graph.add_vertex(v)
 
-		return reversed_graph
+            for u in self.get_neighbors(v):
+                reversed_graph.add_edge(u, v)
 
-	def has_edge(self, u, v):
-		return v in self.graph[u]
+        return reversed_graph
 
-	def has_vertex(self, v):
-		return v in self.graph
+    def has_edge(self, u, v):
+        return v in self.graph[u]
 
-	def get_vertices(self):
-		return self.graph.keys()
+    def has_vertex(self, v):
+        return v in self.graph
 
-	def __str__(self):
-		return str(self.graph)
+    def get_vertices(self):
+        return self.graph.keys()
 
-	def __len__(self):
-		return len(self.graph)
+    def __str__(self):
+        return str(self.graph)
 
-	def __iter__(self):
-		return iter(self.graph)
+    def __len__(self):
+        return len(self.graph)
+
+    def __iter__(self):
+        return iter(self.graph)
 
 
 class AdjacencyList(GraphAbstractRepresentation):
-	"""
-	Adjacency list representation of a graph
-	note::
-		- weights are not supported in this representation, yet.
-	"""
-	def __init__(self, directed=False, weight=1):
-		super().__init__(directed, weight)
-		self.graph = defaultdict(list)
+    """
+    Adjacency list representation of a graph
+    note::
+        - weights are not supported in this representation, yet.
+    """
 
-	def add_edge(self, u, v, weight=1):
-		self.graph[u].append(v)
-		if not self.directed:
-			self.graph[v].append(u)
+    def __init__(self, directed=False, weighted=False):
+        super().__init__(directed, weighted)
+        self.graph: dict[dict | list] = defaultdict(dict if self.weighted else list)
 
-	def add_vertex(self, v):
-		self.graph[v] = []
+    def add_edge(self, u, v, weight=1):
+        if self.weighted:
+            self.graph[u][v] = weight
+            if not self.directed:
+                self.graph[v][u] = weight
+            return
 
-	def remove_edge(self, u, v):
-		self.graph[u].remove(v)
-		if not self.directed:
-			self.graph[v].remove(u)
+        self.graph[u].append(v)
+        if not self.directed:
+            self.graph[v].append(u)
 
-	def get_neighbors(self, u):
-		return self.graph[u]
+    def add_vertex(self, v):
+        self.graph[v] = dict() if self.weighted else list()
 
-	def reverse_graph(self):
-		return self.reverse_helper(AdjacencyList(directed=True))
+    def remove_edge(self, u, v):
+        if self.weighted:
+            del self.graph[u][v]
+            if not self.directed:
+                del self.graph[v][u]
+            return
+
+        self.graph[u].remove(v)
+        if not self.directed:
+            self.graph[v].remove(u)
+
+    def get_edge_weight(self, u, v):
+        if not self.weighted or not self.has_edge(u, v):
+            raise ValueError("Graph is not weighted or edge doesn't exist")
+        return self.graph[u][v]
+
+    def get_neighbors(self, u, weights_only=False):
+        if not weights_only:
+            return self.graph[u]
+        if not self.weighted and weights_only:
+            raise ValueError(f"This graph isn't weighted")
+
+        return [weight for weight in self.graph[u].values()]
+
+    def reverse_graph(self):
+        if not self.directed:
+            raise ValueError("Graph is not directed")
+        return self.reverse_helper(AdjacencyList(directed=self.directed, weighted=self.weighted))
 
 
 class AdjacencyMatrix(GraphAbstractRepresentation):
-	"""
-	Adjacency matrix representation of a graph
-	"""
-	def __init__(self, directed=False, weight=1):
-		super().__init__(directed, weight)
-		self.graph = defaultdict(dict)
+    """
+    Adjacency matrix representation of a graph
+    todo::
+        - Make sure that this representation is working correctly, and represent Adjacency Matrix.
+    """
 
-	def add_edge(self, u, v, weight=1):
-		self.graph[u][v] = weight
-		if not self.directed:
-			self.graph[v][u] = weight
+    def __init__(self, directed=False, weighted=False):
+        super().__init__(directed, weighted)
+        self.graph = defaultdict(dict)
 
-	def add_vertex(self, v):
-		self.graph[v] = dict()
+    def add_edge(self, u, v, weight=1):
+        self.graph[u][v] = weight
+        if not self.directed:
+            self.graph[v][u] = weight
 
-	def remove_edge(self, u, v):
-		del self.graph[u][v]
-		if not self.directed:
-			del self.graph[v][u]
+    def add_vertex(self, v):
+        self.graph[v] = dict()
 
-	def get_neighbors(self, u):
-		return self.graph[u].keys()
+    def remove_edge(self, u, v):
+        del self.graph[u][v]
+        if not self.directed:
+            del self.graph[v][u]
 
-	def reverse_graph(self):
-		return self.reverse_helper(AdjacencyMatrix(directed=True))
+    def get_edge_weight(self, u, v):
+        return self.graph[u][v]
+
+    def get_neighbors(self, u, weights_only=False):
+        return self.graph[u].keys()
+
+    def reverse_graph(self):
+        if not self.directed:
+            raise ValueError("Graph is not directed")
+        return self.reverse_helper(AdjacencyMatrix(directed=self.directed, weighted=self.weighted))
 
 
 class Graph:
-	"""
-	Graph class that uses the adjacency list representation by default
-	note::
-		- Graph class isn't thread safe, yet!
-	"""
-	def __init__(self, representation=AdjacencyList, directed=False, weight=1):
-		self.representation = representation(directed, weight)
-		self.graph = self.representation.graph
+    """
+    Graph class that uses the adjacency list representation by default
+    note::
+        - Graph class isn't thread safe, yet!
+    """
 
-	def add_edge(self, u, v, weight=1):
-		self.representation.add_edge(u, v, weight)
+    def __init__(self, representation=AdjacencyList, directed=False, weighted=False):
+        self.representation = representation(directed, weighted)
 
-	def add_vertex(self, v):
-		self.representation.add_vertex(v)
+    def add_edge(self, u, v, weight=1):
+        self.representation.add_edge(u, v, weight)
 
-	def remove_edge(self, u, v):
-		self.representation.remove_edge(u, v)
+    def add_vertex(self, v):
+        self.representation.add_vertex(v)
 
-	def has_edge(self, u, v):
-		return self.representation.has_edge(u, v)
+    def remove_edge(self, u, v):
+        self.representation.remove_edge(u, v)
 
-	def has_vertex(self, v):
-		return self.representation.has_vertex(v)
+    def get_edge_weight(self, u, v):
+        return self.representation.get_edge_weight(u, v)
 
-	def get_vertices(self):
-		return self.representation.get_vertices()
+    def has_edge(self, u, v):
+        return self.representation.has_edge(u, v)
 
-	def get_neighbors(self, u):
-		return self.representation.get_neighbors(u)
+    def has_vertex(self, v):
+        return self.representation.has_vertex(v)
 
-	def reverse_graph(self):
-		return self.representation.reverse_graph()
+    def get_vertices(self):
+        return self.representation.get_vertices()
 
-	def __len__(self):
-		return len(self.representation)
+    def get_neighbors(self, u, weights_only=False):
+        return self.representation.get_neighbors(u, weights_only)
+
+    def reverse_graph(self):
+        return self.representation.reverse_graph()
+
+    def __len__(self):
+        return len(self.representation)
--- part_2/shortest_paths.py
+++ part_2/shortest_paths.py
@@ -1,36 +1,84 @@
 import math
 from queue import Queue
 
+from graph_representation import Graph
+
 
 class ShortestPaths:
-	def __init__(self, graph):
-		self.__distances = {}
-		self.graph = graph.graph
+    def __init__(self, graph):
+        self.__distances = {}
+        self.graph = graph.graph
 
-	def shortest_path(self, vertex):
-		visited = set(vertex)
-		length = {k: math.inf for k in self.graph}
-		length[vertex] = 0
-		queue = Queue()
-		queue.put(vertex)
-		while not queue.empty():
-			vertex = queue.get()
-			for edge in self.graph[vertex]:
-				if edge not in visited:
-					visited.add(edge)
-					length[edge] = length[vertex] + 1
-					queue.put(edge)
-		return length
+    def shortest_path(self, vertex):
+        visited = set(vertex)
+        length = {k: math.inf for k in self.graph}
+        length[vertex] = 0
+        queue = Queue()
+        queue.put(vertex)
+        while not queue.empty():
+            vertex = queue.get()
+            for edge in self.graph[vertex]:
+                if edge not in visited:
+                    visited.add(edge)
+                    length[edge] = length[vertex] + 1
+                    queue.put(edge)
+        return length
 
-	def __call__(self, graph, source):
-		self.__distances = {vertex: float('inf') for vertex in graph.graph}
-		self.__distances[source] = 0
-		queue = Queue()
-		queue.put(source)
-		while not queue.empty():
-			vertex = queue.get()
-			for neighbor in graph.get_neighbors(vertex):
-				if self.__distances[neighbor] == float('inf'):
-					self.__distances[neighbor] = self.__distances[vertex] + 1
-					queue.put(neighbor)
-		return self.__distances
+    def __call__(self, graph, source):
+        self.__distances = {vertex: float("inf") for vertex in graph}
+        self.__distances[source] = 0
+        queue = Queue()
+        queue.put(source)
+        while not queue.empty():
+            vertex = queue.get()
+            for neighbor in graph.get_neighbors(vertex):
+                if self.__distances[neighbor] == float("inf"):
+                    self.__distances[neighbor] = self.__distances[vertex] + 1
+                    queue.put(neighbor)
+        return self.__distances
+
+
+class SingleSourceShortestPath:
+    def __init__(self, graph):
+        self.graph = graph
+        self.visited = set()
+        self.distances = dict()
+
+    def dijkstra_algorithm(self, source, destination):
+        self.visited.add(source)
+        self.distances = {vertex: float("inf") for vertex in self.graph.get_vertices()}
+        self.distances[source] = 0
+
+        while destination not in self.visited:
+            min_distance = float("inf")
+            next_vertex = None
+
+            for vertex in self.visited:
+                remaining_edges = set(self.graph.get_neighbors(vertex)) - self.visited
+                for edge in remaining_edges:
+                    dist = self.distances[vertex] + self.graph.get_neighbors(vertex)[edge]
+                    if min_distance > dist:
+                        min_distance = dist
+                        next_vertex = edge
+
+            self.visited.add(next_vertex)
+            self.distances[next_vertex] = min_distance
+
+        return self.distances[destination]
+
+
+if __name__ == "__main__":
+    graph_input = Graph(weighted=True)
+
+    with open("dijkstra's algorithm.txt", "r") as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            line = lines[i].split()
+            for index in range(1, len(line)):
+                edge, weight = line[index].split(",")
+                graph_input.add_edge(line[0], edge, int(weight))
+
+    destinations = ["7", "37", "59", "82", "99", "115", "133", "165", "188", "197"]
+    for destination in destinations:
+        shortest_path = SingleSourceShortestPath(graph=graph_input)
+        print(shortest_path.dijkstra_algorithm("1", destination), end=",")
